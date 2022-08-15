8d48d70667ab159b173d4206876c41d33b7959de
Korytov Pavel <thexcloud@gmail.com>
2022-08-15 21:02:32 +0500

    Initial commit

---
+++ reverso.el
@@ -0,0 +1,36 @@
+;;; reverso.el --- Client for the https://reverso.net translation service -*- lexical-binding: t -*-
+
+;; Copyright (C) 2022 Korytov Pavel
+
+;; Author: Korytov Pavel <thexcloud@gmail.com>
+;; Maintainer: Korytov Pavel <thexcloud@gmail.com>
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "27.1") (transient "0.3.7") (request "0.3.2"))
+;; Homepage: https://github.com/SqrtMinusOne/reverso.el
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; TODO
+
+;;; Code:
+(require 'request)
+(require 'transient)
+
+
+(provide 'reverso)
+;;; reverso.el ends here
